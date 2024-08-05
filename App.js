a92ebdead1b24a50386eba86139a9a8a385bbb78
Jashangill1 <jashanpreet.singh03@edu.sait.ca>
2024-08-05 13:27:48 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import ChooseLevelScreen from './screens/ChooseLevelScreen';
+import QuizScreen from './screens/QuizScreen';
+import ResultScreen from './screens/ResultScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Choose Level" component={ChooseLevelScreen} />
+        <Stack.Screen name="Quiz" component={QuizScreen} />
+        <Stack.Screen name="Result" component={ResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
